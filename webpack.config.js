a371cc37d4c9658248d02dd8bc669577bcaf16e1
Fabian Benavides <jfbenavid@hotmail.com>
2019-08-22 20:05:58 -0500

    Finished the template using eslint and webpack for react

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "name.js"
+    filename: 'bundle.js'
   },
   plugins: [
-		new HtmlWebpackPlugin()
-	],
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
     ]
   }
-};
+}
